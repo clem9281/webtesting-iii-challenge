f4fcf1c26f4c71394659ced1e5a79e825d49d6c0
clem9281 <clem9281@gmail.com>
2019-04-10 12:39:06 -0700

    added verbose flag to test script, wrote one test to make sure the dashboard matches it's snapshot

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
 // Test away
+import React from "react";
+import {render } from "react-testing-library";
+import renderer from "react-test-renderer";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe('Dashboard', () => {
+    it("should match the snapshot", () => {
+       const tree = renderer.create(<Dashboard />).toJSON();
+       expect(tree).toMatchSnapshot();
+    })
+})
+
